237e2c16ef85711f24bd6f9ab2887b71ba5f79a2
Mikkel Rask <mikkelrask@protonmail.com>
2020-02-04 16:08:20 +0100

    Abuse email finder added

---
+++ abusemail.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+clear
+
+whois $1 | grep abuse
--- scammer.py
+++ scammer.py
@@ -4,20 +4,57 @@ import random
 import string
 import json
 
+print('mmmmmmm #                     mmmm                                           ')
+print('   #    # mm    mmm          #"   " mmmm    mmm   mmmmm  mmmmm   mmm    m mm ')
+print('   #    #"  #  #"  #         "#mmm  #" "#  "   #  # # #  # # #  #"  #   #"  "')
+print('   #    #   #  #""""             "# #   #  m"""#  # # #  # # #  #""""   #    ')
+print('   #    #   #  "#mm"         "mmm#" ##m#"  "mm"#  # # #  # # #  "#mm"   #    ')
+print('                                    #                                        ')
+print('                                    "                                        ')
+print('')
+print('*****************************************************************************')
+print('')
+print('Please use with care. Do not spam websites, that arent phishing for data.')
+print('You will need to find the POST URL, username input and password input ')
+print('before continuing.')
+print('See https://github.com/mikkelrask/python-scammer-spammer for details.')
+print('')
+print('*****************************************************************************')
+print('')
+
 chars = string.ascii_letters + string.digits + '!@#$%^&*()'
 random.seed = (os.urandom(1024))
 
-#url = 'https://innoique.icu/prv/3f5c944e6d930973cedf945fa1c3bdad647/loginerror.php'
-#url = 'https://mediation-de.com/prv/12f6009a33bccd92c33a9369248ff983866/loadlogin.php'
-url  = 'http://jacobderusha.com/prv/97fd1ae641100c6994d55e4121aaa5dd189/loadlogin.php'
+# What are we going to send?
+print('What do we wish to send today?')
+print('1: Usernames and password')
+print('2: Email-address and passwords')
+print('3: Social security numbers and passwords')
+print('')
+userformat = int(input('Please pick: '))
+print('')
+url  		= input('Please inset POST URL that requests data: ')
+print('')
+userinput 	= input('Name of the username input: ')
+print('')
+passwinput 	= input('Name of the password input: ')
+print('')
+datanumbers = int(input('How many logins do you wish to send: '))
+print('')
 
 names = json.loads(open('names.json').read())
 
-for name in names:
-	#name_extra = ''.join(random.choice(string.digits))
 
-	#username = name.lower() + name_extra
+i = 1
+while(i<datanumbers):
+	#Username / Email generation
+	for name in names:
+		name_extra = ''.join(random.choice(string.digits))
+		username = name.lower() + name_extra
+		username = name.lower() + name_extra
+		email = username + '@live.dk'
 
+	#Danish SSN generator
 	DD = random.randint(1,30) # Max 30  
 	MM = random.randint(1,12) # Max 12
 	YYYY = random.randint(1940,2002) # Max 2002
@@ -29,12 +66,30 @@ for name in names:
 	if(MM<10):
 		MM = str("0")+ str(MM)
 	
-	username = str(DD) + str(MM) + str(YYYY) + str(EXTRA)
+	ssn = str(DD) + str(MM) + str(YYYY) + str(EXTRA)
+
+	# Password generator
 	password = ''.join(random.choice(chars) for i in range(10))
 
+	if(userformat == 1):
+		choice = username
+	elif(userformat == 2):
+		choice = email
+		username = email
+	elif(userformat == 3):
+		username = ssn
+
 	requests.post(url, allow_redirects=False, data={
-		'userid': username,
-		'password': password,
-		'submit': 'submit'
+		userinput: username,
+		passwinput: password,
+		'submit': ''
 	})
-	print("sending social security number " + username + " and password " + password)
+	print("sending login " + username + " and password " + password)
+
+print('')
+print('Successfully sent ' + str(datanumbers) + ' fake random logins to the POST url ' + url)
+print('*****************************************************************************')
+print("Please consider reporting this URL to the google report phishing form found ")
+print("here to help warn others that aren/'t as techsavvy as you - thanks!")
+print('')
+print('https://safebrowsing.google.com/safebrowsing/report_phish/')

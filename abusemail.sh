9398c155b8ece59821690a6d5b5800672665db35
Mikkel Rask <mikkelrask@protonmail.com>
2020-02-04 16:35:27 +0100

    updated whois script a bit

--- abusemail.sh
+++ abusemail.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 clear
-
+echo "WHOIS information provided by whois.com "
+echo ""
+echo "****************************************"
+echo $1
+echo "****************************************"
+echo ""
 whois $1 | grep abuse
